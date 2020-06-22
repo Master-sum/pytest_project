3f0db5f21d87897f94c7cf60723d743e9d1bd7be
‘Master-sum’ <285957109@qq.com>
2020-06-22 23:08:33 +0800

    1.0版本pytest项目

---
+++ tasks_proj/tests/func/test_add.py
@@ -0,0 +1,14 @@
+"""
+Placeholder test file.
+
+We'll add a bunch of tests here in later versions.
+"""
+import pytest
+
+def test_add():
+    """Placeholder test."""
+    assert 1 != 1
+
+
+if __name__ == '__main__':
+    pytest.main(['-s','-p',"test_add"])
